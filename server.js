eda1843dbaa6c9d8571010063095e646fc935acf
wawo99 <panaq2009@gmail.com>
2024-08-08 19:32:10 +0900

    55

--- server.js
+++ server.js
@@ -63,8 +63,85 @@ io.on('conncetion', socket => {
         })
     })
 
-    socket.on('checked dat')
+    socket.on('checked date list', () => {
+        getCheckedList()
+    })
 
+    socket.on('checked remove date', ({date}) => {
+        db.checkdate.remove({date}, {multi: false})
+        getCheckedList()
+    })
 
+    socket.on('remove room', ({name}) => {
+        setRooms((prev) => prev.filter((item) => item !== name))
+    })
+
+    socket.on('join room', ({preRoom, newRoom, name}) => {
+        console.log('입장')
+        socket.name = name
+        socket.join(newRoom)
+        socket.room = newRoom
+        let clients = io.sockets.adapter.rooms.get(newRoom)
+        const {currentChatRoomUserList, roomClientsNum} = getRoomInfo(clients)
+
+        const chatRoomList = getUserRooms()
+        io.emit('chatRoomList', chatRoomList)
+        io.emit('notice', {currentChatRoomUserList, roomClientsNum, name: socket.name})
+    })
+
+    socket.on('get room list', () => {
+        const chatRoomList = getUserRooms()
+        io.emit('chatRoomList', chatRoomList)
+    })
         
 })
+
+function getToday() {
+    const ts = Date.now()
+    const dateTime = new Date(ts)
+    const date = dateTime.getDate()
+    const month = dateTime.getMonth() + 1
+    const year = dateTime.getFullYear()
+    return `${year}${month}${date}`
+}
+
+function getTodayChatFile(type) {
+    const fileName = `${type}${getToday()}.db`
+    const filtPath = `${__dirname}/data/${fileName}`
+    return filePath
+}
+
+function getRoomInfo(clients) {
+    const roomClientsNum = clients ? clients.size : 0
+    const currentChatRoomUserList = []
+    if(clients){
+        clients.forEach(element => {
+            currentChatRoomUserList.push(io.sockets.sockets.get(element).name)
+        })
+    }
+
+    return { roomClientsNum, currentChatRoomUserList }
+}
+
+const getUserRooms = () => {
+    const userRooms = []
+    const { sids, rooms } = io.of('/').adapter
+    rooms.forEach((_, key) => {
+        if(sids.get(key) === undefined){
+            userRooms.push(key)
+        }
+    })
+    return userRooms
+}
+
+function selectList() {
+    db.checkdate.find({}, (e,v)=>{
+        io.emit('checked date list', v)
+    })
+}
+
+function getCheckedList() {
+    const filePath = `${__dirname}/data/checkDate.db`
+    db.checkdate = new Datastore({filename: filePath, autoload: true})
+    selectList()
+}
